46ff8279ff91e6cbf4952aab9a4b8e462ae3539e
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-17 12:06:10 +0800

    markup completed

---
+++ src/components/friendList/FriendList.jsx
@@ -0,0 +1,21 @@
+import FriendListItem from "../friendListItem/FriendListItem";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map((friend) => {
+        return (
+          <li key={friend.id}>
+            <FriendListItem
+              avatar={friend.avatar}
+              name={friend.name}
+              isOnline={friend.isOnline}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FriendList;
